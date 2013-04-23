8d3303f6dca971e32828f5771e894d3480c6d3fd
Gary Lai <garylai1990@gmail.com>
2013-04-23 23:19:08 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncku102graduation::Application.config.secret_token = '76f1de337d1e249b80604be7bef33dce3ebaa9716bd4d50b4d52a46c109b2384059d48e6a374e6624eed0dbf8005e3bdbc69d281b8095eb3a376dc562f863756'
