5bf7c7e5c4b51086ff04c3671dfdb44d8515703d
Gary Lai <garylai1990@gmail.com>
2013-04-30 20:38:29 +0800

    add author

--- app/assets/javascripts/toggle.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-	
-    jQuery(".toggle").next(".map_h").hide();
-    jQuery(".toggle").click(function() {
-        $('.active').not(this).toggleClass('active').next('.map_h').slideToggle(300);
-        $(this).toggleClass('active').next().slideToggle("fast");
-
-    });
-});
